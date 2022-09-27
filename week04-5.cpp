eb1b847801afcb6f77606fbb19aa4351920b199a
Abby <112918857+yiiiiii1201@users.noreply.github.com>
2022-09-27 11:59:48 +0800

    week04

---
+++ week04-5.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int N=7;
+    for(int i=0;i<N;i++){
+        printf("出現i:%d\n",i);
+    }
+    for(int i=1;i<=N;i++){
+        printf("接下來i:%d\n",i);
+    }
+}
