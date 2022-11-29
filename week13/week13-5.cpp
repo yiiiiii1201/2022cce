dfddfbd83af8da141a3235b1af469a488a1ae34a
yiiiiii1201 <chen20031201@gmail.com>
2022-11-29 12:03:31 +0800

    week13

---
+++ week13/week13-5.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int a[10][100];
+
+int main()
+{
+    int n,m;
+    scanf("%d",&n,&m);
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<m;j++){
+            scanf("%d",&a[i][j]);
+        }
+    }
+    printf("\n");
+    for(int j=0;j<m;j++){
+        for(int i=n-1;i>=0;i--){
+            printf("%2d ",a[i][j]);
+        }
+        printf("\n");
+    }
+}
